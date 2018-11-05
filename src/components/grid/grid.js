0556c974b5c8c0a6ee1fd6050733df1341082b36
stefanfranzen <stefan.franzn@gmail.com>
2018-11-06 00:21:31 +0100

    Initial commit

---
+++ src/components/grid/grid.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './grid.css';
+
+const Grid = function () {
+  return (
+    <div class="container">
+      <div class="header">
+        <h2>Business Model Canvas - Team Continuous</h2>
+      </div>
+      <div class="key-partners"><p>Key Partners</p></div>
+      <div class="key-activities"><p>Key Activities</p></div>
+      <div class="key-resources"><p>Key Resources</p></div>
+      <div class="value-propositions"><p>Value Propositions</p></div>
+      <div class="customer-relationships"><p>Customer Relationships</p></div>
+      <div class="channels"><p>Channels</p></div>
+      <div class="customer-segments"><p>Customer Segments</p></div>
+      <div class="cost-structures"><p>Cost Structure</p></div>
+      <div class="revenue-streams"><p>Revenue Streams</p></div>
+    </div>
+  );
+}
+
+export default Grid;
---
+++ src/components/navigation-bar/nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './nav.css';
+
+const Nav = () => {
+  return (
+    <div>
+      <div className="nav">Login-button</div>
+    </div>
+  )
+}
+
+export default Nav;
