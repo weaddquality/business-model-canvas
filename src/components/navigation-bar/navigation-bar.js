5f0f6b4f18708871691d4cdc48fe2b697acffd08
stefanfranzen <stefan.franzn@gmail.com>
2019-02-08 12:19:15 +0100

    Add glyphicons for signup, log-in and log-out buttons. Also bump versions for react packages in package.json

--- src/components/navigation-bar/navigation-bar.js
+++ src/components/navigation-bar/navigation-bar.js
@@ -1,11 +1,23 @@
 import React, { Fragment } from 'react'
 import './navigation-bar.css'
 import { Link } from 'react-router-dom'
-import { Nav, Navbar, NavItem } from 'react-bootstrap'
+import {
+  Nav,
+  Navbar,
+  NavItem,
+  ButtonToolbar,
+  DropdownButton,
+  Dropdown,
+  Button,
+} from 'react-bootstrap'
+// import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
+// import Dropdown from 'react-bootstrap/lib/Dropdown';
+// import DropdownButton from 'react-bootstrap/lib/DropdownButton';
+// import DropdownItem from 'react-bootstrap/lib/DropdownItem';
 
 const NavigationBar = ({ props }) => {
   return (
-    <Navbar fluid collapseOnSelect>
+    <Navbar fluid collapseOnSelect className="navbar navbar-inverse">
       <Navbar.Header>
         <Navbar.Brand>
           <Link to="/">Business Model Canvas</Link>
@@ -15,14 +27,20 @@ const NavigationBar = ({ props }) => {
       <Navbar.Collapse>
         <Nav pullRight>
           {props.isAuthenticated ? (
-            <NavItem onClick={props.handleLogout}>Logout</NavItem>
+            <NavItem onClick={props.handleLogout}>
+              <span className="glyphicon glyphicon-log-out" /> Logout
+            </NavItem>
           ) : (
             <Fragment>
               <NavItem>
-                <Link to="/signup">Signup</Link>
+                <Link to="/signup">
+                  <span className="glyphicon glyphicon-user" /> Signup
+                </Link>
               </NavItem>
               <NavItem>
-                <Link to="/login">Login</Link>
+                <Link to="/login">
+                  <span className="glyphicon glyphicon-log-in" /> Login
+                </Link>
               </NavItem>
             </Fragment>
           )}
