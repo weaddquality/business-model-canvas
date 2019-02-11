1030de57782d4467685ed1b80bf55a782cf5f6e3
stefanfranzen <stefan.franzn@gmail.com>
2019-02-11 11:30:51 +0100

    Improve experience of reading cards on different resolutions on the canvas and horizontal page.

--- src/components/canvas-block/canvas-block.js
+++ src/components/canvas-block/canvas-block.js
@@ -8,7 +8,6 @@ const headerSubString = item => {
   if (item.length > maxCharacters) {
     item = `${item.substring(0, maxCharacters)}...`
   }
-  console.log(item)
   return item
 }
 
@@ -17,21 +16,22 @@ const textSubString = item => {
   if (item.length > maxCharacters) {
     item = `${item.substring(0, maxCharacters)}...`
   }
-  console.log(item)
   return item
 }
 
 const CanvasBlock = function(props) {
   const items = props.content.items.map(item => {
     return (
-      <Card border="dark" className="canvas-item-card">
-        <Card.Header className="canvas-item-header">
-          {headerSubString(item.header)}
-        </Card.Header>
-        <Card.Body>
-          <Card.Text>{textSubString(item.text)}</Card.Text>
-        </Card.Body>
-      </Card>
+      <div className="canvas-card-container">
+        <Card border="dark" className="canvas-item-card">
+          <Card.Header className="canvas-item-header">
+            {headerSubString(item.header)}
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>{textSubString(item.text)}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     )
   })
 
