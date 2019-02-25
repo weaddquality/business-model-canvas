eac658330bda9371836cff56788c0c41d64b2e8f
Måns Nilsson <mans.woxulv@gmail.com>
2019-02-25 23:52:06 +0100

    Refactor the App component into a function component

--- src/App.js
+++ src/App.js
@@ -1,70 +1,68 @@
 import Routes from './Routes'
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import './components/navigation-bar/Navigation-bar.css'
 import { withRouter } from 'react-router-dom'
 import NavigationBar from './components/navigation-bar/Navigation-bar'
 import { Auth } from 'aws-amplify'
 
-class App extends Component {
-  state = {
-    isAuthenticated: false,
-    isAuthenticating: true,
-    isHorizontalViewToggled: false,
-  }
+function App(props) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticating, setIsAuthenticating] = useState(true)
 
-  async componentDidMount() {
+  useEffect(() => {
+    userHasSession()
+  }, [])
+
+  const userHasSession = async () => {
     try {
       await Auth.currentSession()
-      this.userHasAuthenticated(true)
+      userHasAuthenticated(true)
     } catch (e) {
       if (e !== 'No current user') {
         alert(e)
       }
     }
 
-    this.setState({ isAuthenticating: false })
+    handleIsAuthenticating(false)
   }
 
-  toggleHorizontalView = () => {
-    this.setState({ isHorizontalViewToggled: !this.state.isHorizontalViewToggled })
+  const handleIsAuthenticating = authenticating => {
+    setIsAuthenticating(authenticating)
   }
 
-  userHasAuthenticated = authenticated => {
-    this.setState({ isAuthenticated: authenticated })
+  const userHasAuthenticated = authenticated => {
+    setIsAuthenticated(authenticated)
   }
 
-  handleLogout = async () => {
+  const handleLogout = async () => {
     await Auth.signOut()
-    this.userHasAuthenticated(false)
-    this.props.history.push('/logout')
+    userHasAuthenticated(false)
+    props.history.push('/logout')
   }
 
-  redirectToLogin = () => {
-    this.props.history.push('/login')
+  const redirectToLogin = () => {
+    props.history.push('/login')
   }
 
-  render() {
-    const childProps = {
-      isAuthenticated: this.state.isAuthenticated,
-      userHasAuthenticated: this.userHasAuthenticated,
-      handleLogout: this.handleLogout,
-      isHorizontalViewToggled: this.state.isHorizontalViewToggled,
-      toggleHorizontalView: this.toggleHorizontalView,
-      redirectToLogin: this.redirectToLogin,
-    }
-    return (
-      !this.state.isAuthenticating && (
-        <div className="App">
-          <div className="background-container">
-            <div className="background-image" />
-            <NavigationBar props={childProps} />
-            <Routes props={childProps} />
-          </div>
+  const childProps = {
+    isAuthenticated: isAuthenticated,
+    userHasAuthenticated: userHasAuthenticated,
+    handleLogout: handleLogout,
+    redirectToLogin: redirectToLogin,
+  }
+
+  return (
+    !isAuthenticating && (
+      <div className="App">
+        <div className="background-container">
+          <div className="background-image" />
+          <NavigationBar props={childProps} />
+          <Routes props={childProps} />
         </div>
-      )
+      </div>
     )
-  }
+  )
 }
 
 export default withRouter(App)
