ac9cd5840cdd8f6ec9d488faf3c0e5acbcbfe010
stefanfranzen <stefan.franzn@gmail.com>
2019-02-28 00:57:37 +0100

    Get the first items for canvas through the backend-api

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Canvas.css'
 import CanvasBlock from '../canvas-block/Canvas-block'
 import blocks from '../canvas-block/content'
+import { API } from 'aws-amplify'
 
 const Canvas = () => {
+  const [listResponse, setListResponse] = useState({ items: [] })
+
+  useEffect(() => {
+    getItems()
+  }, [])
+
+  const getItems = () => {
+    API.get('bmc-items', '/bmc-items').then(response => {
+      const returnedResponse = {
+        items: [
+          { header: response[0].ItemHeader, text: response[0].ItemText },
+          { header: response[0].ItemHeader, text: response[0].ItemText },
+          { header: response[0].ItemHeader, text: response[0].ItemText },
+        ],
+      }
+      setListResponse(returnedResponse)
+    })
+  }
+
   const canvasBlocks = blocks.map(block => {
-    return <CanvasBlock key={block.className} content={block} />
+    return <CanvasBlock key={block.className} content={listResponse} />
   })
   return (
-    <div className="canvas-view canvas-horizontal-view" data-testid="canvasView">
-      {canvasBlocks}
+    <div>
+      <div className="canvas-view canvas-horizontal-view" data-testid="canvasView">
+        {canvasBlocks}
+      </div>
     </div>
   )
 }
