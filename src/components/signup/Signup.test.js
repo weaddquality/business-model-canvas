3f77392e96e9a301b26f82bc1d6b0a66758397b2
Måns Nilsson <mans.woxulv@gmail.com>
2019-03-12 00:15:55 +0100

    Add jest-in-case and a first unit test

---
+++ src/components/signup/Signup.test.js
@@ -0,0 +1,74 @@
+import { validateForm } from './Signup'
+import cases from 'jest-in-case'
+
+cases(
+  'validateForm(email, password, confirmPassword)',
+  opts => {
+    expect(validateForm(opts.email, opts.password, opts.confirmPassword)).toBe(opts.result)
+  },
+  [
+    {
+      name: 'correct credentials',
+      email: 'stefan.franzen@addq.se',
+      password: 'ADDQbmc123!',
+      confirmPassword: 'ADDQbmc123!',
+      result: true,
+    },
+    {
+      name: 'passwords mismatching',
+      email: 'stefan.franzen@addq.se',
+      password: 'ADDQbmc123!',
+      confirmPassword: 'ADDQbmc123',
+      result: false,
+    },
+    {
+      name: 'all arguments are missing',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      result: false,
+    },
+    {
+      name: 'email missing',
+      email: '',
+      password: 'ADDQbmc123!',
+      confirmPassword: 'ADDQbmc123!',
+      result: false,
+    },
+    {
+      name: 'password missing',
+      email: 'stefan.franzen@addq.se',
+      password: '',
+      confirmPassword: 'ADDQbmc123!',
+      result: false,
+    },
+    {
+      name: 'confirm password missing',
+      email: 'stefan.franzen@addq.se',
+      password: 'ADDQbmc123!',
+      confirmPassword: '',
+      result: false,
+    },
+    {
+      name: 'email does not match "@addq.se"',
+      email: 'stefan.franzen@addq.s',
+      password: 'ADDQbmc123!',
+      confirmPassword: 'ADDQbmc123!',
+      result: false,
+    },
+    {
+      name: 'email ends with "@addq.se"',
+      email: 'stefan.franzen@addq.se',
+      password: 'ADDQbmc123!',
+      confirmPassword: 'ADDQbmc123!',
+      result: true,
+    },
+    {
+      name: 'email lack a leading character before "@addq.se"',
+      email: '@addq.se',
+      password: 'ADDQbmc123!',
+      confirmPassword: 'ADDQbmc123!',
+      result: false,
+    },
+  ]
+)
