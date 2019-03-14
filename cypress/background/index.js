8509ee3be3f7132cfe0b101c1707a97e5b0d1ca0
Måns Nilsson <mans.nilsson@thomascook.se>
2019-03-14 16:22:24 +0100

    Add transpilation as Cypress plugin, fix canvas-block test and temporarily change <Link> component to <a> tag

--- cypress/background/index.js
+++ cypress/background/index.js
@@ -1,1 +1,31 @@
-module.exports = (on, config) => {}
+// cypress/plugins/index.js
+const webpack = require('@cypress/webpack-preprocessor')
+const webpackOptions = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|mjs)$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: [/node_modules/],
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ],
+  },
+}
+
+const options = {
+  // send in the options from your webpack.config.js, so it works the same
+  // as your app's code
+  webpackOptions,
+  watchOptions: {},
+}
+
+module.exports = on => {
+  on('file:preprocessor', webpack(options))
+}
--- cypress/tests/components/canvas-block.js
+++ cypress/tests/components/canvas-block.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import 'cypress-react-unit-test'
-import { CanvasBlock } from '../../../src/components/canvas-block/Canvas-block'
+import CanvasBlock from '../../../src/components/canvas-block/Canvas-block'
 
-const props = {
-  content: {
-    items: [
-      {
-        itemHeader: 'my header',
-        itemText: 'my text',
-      },
-    ],
-  },
+const content = {
+  block: 'Value Propositions',
+  items: [
+    {
+      itemHeader: 'my header',
+      ItemText: 'my text',
+    },
+  ],
 }
 
 describe('Component testing of CanvasBlock', () => {
   it('renders', () => {
-    cy.mount(<CanvasBlock props={props} />)
+    cy.mount(<CanvasBlock content={content} />)
     cy.contains('my header')
   })
 })
