be9e3b215df27d3b34d9f8c1906dcf76b07fb5e9
Stefan Franzén <stefan.franzn@gmail.com>
2019-03-16 15:14:23 +0100

    Add a list in the css-grid for the details page

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -11,6 +11,11 @@ export default function Details() {
         <div className="details-text">This is where the text goes</div>
         <div className="details-cancel">Cancel-button</div>
         <div className="details-submit">Submit-button</div>
+        <div className="details-list">
+          <div className="details-list-item">item1</div>
+          <div className="details-list-item">item2</div>
+          <div className="details-list-item">item3</div>
+        </div>
       </div>
       <Link to="/canvas">Go back to Canvas</Link>
     </div>
