d3477e0376488ee36f22c85d2223c2a7165825ff
Stefan Franzén <stefan.franzn@gmail.com>
2019-03-17 14:22:47 +0100

    Add components for buttons as well as minor css-changes on details-component

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -2,31 +2,41 @@ import React from 'react'
 import './Details.css'
 import { Link } from 'react-router-dom'
 import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
 
 export default function Details() {
   return (
     <div>
-      <div className="details-block">Value Propositions</div>
-      <div className="details-form">
-        <div className="details-header">Header here</div>
-        <div className="details-text">This is where the text goes</div>
-        <div className="details-cancel">Cancel-button</div>
-        <div className="details-submit">Submit-button</div>
-        <div className="details-list">
-          <ListGroup>
-            <ListGroup.Item action href="/0109ad10-4783-11e9-824b-1fca161f126b">
-              Item 1
-            </ListGroup.Item>
-            <ListGroup.Item action href="/014a62c0-4787-11e9-824b-1fca161f126b">
-              Item 2
-            </ListGroup.Item>
-            <ListGroup.Item action href="/05a4ae70-4782-11e9-824b-1fca161f126b">
-              Item 3
-            </ListGroup.Item>
-          </ListGroup>
+      <div className="details-container">
+        <div className="details-form">
+          <div className="details-block">Value Propositions</div>
+          <div className="details-header">Header here</div>
+          <div className="details-text">This is where the text goes</div>
+          <div className="details-delete">
+            <Button variant="danger">Delete</Button>
+          </div>
+          <div className="details-cancel">
+            <Button variant="secondary">Cancel</Button>
+          </div>
+          <div className="details-submit">
+            <Button variant="success">Submit</Button>
+          </div>
+          <div className="details-list">
+            <ListGroup>
+              <ListGroup.Item action href="/0109ad10-4783-11e9-824b-1fca161f126b">
+                Item 1
+              </ListGroup.Item>
+              <ListGroup.Item action href="/014a62c0-4787-11e9-824b-1fca161f126b">
+                Item 2
+              </ListGroup.Item>
+              <ListGroup.Item action href="/05a4ae70-4782-11e9-824b-1fca161f126b">
+                Item 3
+              </ListGroup.Item>
+            </ListGroup>
+          </div>
         </div>
+        <Link to="/canvas">Go back to Canvas</Link>
       </div>
-      <Link to="/canvas">Go back to Canvas</Link>
     </div>
   )
 }
