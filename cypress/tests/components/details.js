296afb1702f3df6ecf0cf3a53c703fbfcd41513d
Stefan Franzén <stefan.franzn@gmail.com>
2019-03-17 20:42:14 +0100

    Add tests for details-page

--- cypress/tests/components/details.js
+++ cypress/tests/components/details.js
@@ -4,16 +4,39 @@ import { StaticRouter } from 'react-router-dom'
 import Details from '../../../src/components/details/Details'
 
 describe('Component testing of Details', () => {
-  it('renders a form with items and a "go back to canvas" link', () => {
+  beforeEach(() => {
     cy.mount(
       <StaticRouter>
         <Details />
       </StaticRouter>
     )
+  })
+
+  it('renders a form for css-grid', () => {
     cy.get('.details-form')
+  })
+
+  it('renders a block', () => {
+    cy.contains('Value Propositions')
+  })
+
+  it('renders a header', () => {
+    cy.contains('Header here')
+  })
+
+  it('renders a text', () => {
+    cy.contains('This is where the text goes')
+  })
+
+  it('renders a items-list', () => {
     cy.get('.details-list a')
       .its('length')
       .should('be.gt', 0)
-    cy.contains(/go back to canvas/i)
+  })
+
+  it('renders a "go back to canvas"-link', () => {
+    cy.contains(/go back to canvas/i).then(element => {
+      expect(element[0].pathname).equals('/canvas')
+    })
   })
 })
