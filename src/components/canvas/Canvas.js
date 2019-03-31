8af0469c232a36bc6979782ad7f029805ee1d930
Måns Nilsson <mans.woxulv@gmail.com>
2019-03-31 10:06:15 +0200

    Fix/details get canvasdata (#43)
    * Move GET (list) request function from Canvas to App
    
    * Add hook to trigger GET for Canvas Data on the Details component, and remove the currentBlock state
    
    * Only trigger getCanvasData if we don't have any canvas data
    
    * Clarify how we match 'current block' with url

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import './Canvas.css'
 import CanvasBlock from '../canvas-block/Canvas-block'
-import { API } from 'aws-amplify'
 
 const Canvas = props => {
   const testId = props.location.pathname === '/canvas' ? 'canvasView' : 'horizontalView'
@@ -9,16 +8,8 @@ const Canvas = props => {
     props.location.pathname === '/canvas' ? 'canvas-view canvas-horizontal-view' : 'horizontal-view'
 
   useEffect(() => {
-    getItems()
+    props.getCanvasData()
   }, [])
-
-  // TODO: Replace hard coded Team value with a dynamically read value
-  const getItems = () => {
-    API.get('bmc-items', '/bmc-items/list?Team=Team Continuous').then(response => {
-      const { blocks } = response
-      props.setListResponse(blocks)
-    })
-  }
 
   const canvasBlocks = props.listResponse.map(block => {
     const className = block.block.toLowerCase().replace(' ', '-')
