1501d07dd33d20eb962fb41296975f18550368ef
Måns Nilsson <mans.woxulv@gmail.com>
2019-04-07 20:44:47 +0200

    Extract aws amplify configuration object to a separate module

--- src/index.js
+++ src/index.js
@@ -5,31 +5,8 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 import Amplify from 'aws-amplify'
-import config from './config'
-
-Amplify.configure({
-  Auth: {
-    mandatorySignIn: false,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
-  },
-  Storage: {
-    region: config.s3.REGION,
-    bucket: config.s3.BUCKET,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-  },
-  API: {
-    endpoints: [
-      {
-        name: 'bmc-items',
-        endpoint: config.apiGateway.URL,
-        region: config.apiGateway.REGION,
-      },
-    ],
-  },
-})
+import aws_exports from './aws-exports'
+Amplify.configure(aws_exports)
 
 ReactDOM.render(
   <Router>
