3da09da7e3e063df90775b52293122601e7b2b47
Stefan Franzén <stefan.franzn@gmail.com>
2019-04-16 10:41:27 +0200

    Add cypress tests for canvas page

---
+++ cypress/tests/e2e/canvas.js
@@ -0,0 +1,45 @@
+describe('Testing the canvas', function() {
+  beforeEach(function() {
+    cy.login()
+    cy.visit('/canvas')
+  })
+
+  it('navigate to canvas from navbar', function() {
+    cy.visit('/')
+    cy.get('[data-testid="navbarDropdownSplitButton"]').click()
+    cy.get('#canvas')
+  })
+
+  it('should have atleast one canvas block', function() {
+    cy.get('#canvas-block')
+  })
+
+  it('should have all block headers', function() {
+    cy.contains('Key Partners')
+    cy.contains('Key Activities')
+    cy.contains('Value Propositions')
+    cy.contains('Customer Relationships')
+    cy.contains('Customer Segments')
+    cy.contains('Key Resources')
+    cy.contains('Channels')
+    cy.contains('Cost Structures')
+    cy.contains('Revenue Streams')
+  })
+
+  it('should have all block descriptions', function() {
+    cy.contains('How do we reach our customer segments')
+    cy.contains('What key activities do our value propositions require')
+    cy.contains('What value do we deliver to the customer')
+    cy.contains('What type of relationships do our customer segments expect')
+    cy.contains('Who are we creating value for')
+    cy.contains('What key resources do our value propositions require')
+    cy.contains('How do we reach our customer segments')
+    cy.contains('What are the important costs inherent in our business model')
+    cy.contains('What value are our customers willing to pay for')
+  })
+
+  it('should be able to click a canvas-block and navigate to the details page', function() {
+    cy.get('#canvas-block').click()
+    cy.get('#details')
+  })
+})
